0740e895291bdc074bd8b08d154e8e3734f2dec8
zora lin <zora.lin@dudooeat.com>
2024-09-08 23:42:44 +0800

    Add homework

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/for_vite_2024/ ',
   plugins: [
     vue(),
     vueDevTools(),
